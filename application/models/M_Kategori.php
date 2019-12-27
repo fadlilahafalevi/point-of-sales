571f47e40ef1a7bbaa1efb9e483f5d92ec2d33aa
Fadlilah Achmad Falevi <fadlilah21.achmad@gmail.com>
2019-12-28 00:50:19 +0700

    update semua master dan fixing sidebar href

--- application/models/M_Kategori.php
+++ application/models/M_Kategori.php
@@ -16,6 +16,30 @@ class M_Kategori extends CI_Model{
 		return $query->result();
 	}
 
+	public function getKategoriByID($kategori_id) {
+		$this->db->select('*');
+		$this->db->from('tbl_kategori');
+		$this->db->where('kategori_id', $kategori_id);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function updateKategori($val){
+        $this->load->model('M_Kategori');
+
+		$data_to_update = array(
+            "kategori_id"			=> $val['kategori_id'],
+            "kategori_nama"			=> $val['kategori_nama']
+        );
+
+		$this->db->where('kategori_id', $val["kategori_id"]);
+        return $this->db->update('tbl_kategori', $data_to_update);
+	}
+
+	public function input_data($data) {
+		$this->db->insert('tbl_kategori', $data);
+	}
+
 	// function simpan_kategori($kat){
 	// 	$hsl=$this->db->query("INSERT INTO tbl_kategori(kategori_nama) VALUES ('$kat')");
 	// 	return $hsl;
